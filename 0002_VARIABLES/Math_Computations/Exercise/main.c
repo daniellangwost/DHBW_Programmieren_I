586387a9d9c3b51104091e9e1818ada35bf0834f
daniellangwost <daniellangwost9@gmail.com>
2025-12-15 15:51:38 +0100

    complete math computations

--- 0002_VARIABLES/Math_Computations/Exercise/main.c
+++ 0002_VARIABLES/Math_Computations/Exercise/main.c
@@ -2,7 +2,21 @@
 
 int main()
 {
+    // definitions and declarations
+    float z1, z2, z3;
+    float x = -2;
+    float y = -3;
 
+    // computations
+    z1 = x*x + y*y - (x*y) + 2;
+    z2 = (x-y) * (x-y) * (x-y) - 3;
+    z3 = (2*(x*x*x) - 0.5*(x*x) - x + 4) / y;
+
+    // output
+    printf("z1: %f\n", z1);
+    printf("z2: %f\n", z2);
+    printf("z3: %f\n", z3);
+    
     return 0;
 }
 
