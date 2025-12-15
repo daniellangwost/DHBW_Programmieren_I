5a85f1c158fdafb11cfaea7190787a66dbd305ea
daniellangwost <daniellangwost9@gmail.com>
2025-12-15 16:01:51 +0100

    implement time converter

--- 0002_VARIABLES/Time_Converter/Exercise/main.c
+++ 0002_VARIABLES/Time_Converter/Exercise/main.c
@@ -2,7 +2,21 @@
 
 int main()
 {
+    float millisconds = 0.0F;
+    float seconds, minutes, hours, days;
 
+    printf("Please enter a millisecond value: ");
+    scanf("%f", &millisconds);
+
+    seconds = millisconds / 1000;
+    minutes = seconds / 60;
+    hours = minutes / 60;
+    days = hours / 24;
+
+    printf("seconds: %f\n", seconds);
+    printf("minutes: %f\n", minutes);
+    printf("hours: %f\n", hours);
+    printf("days: %f\n", days);
 
     return 0;
 }
