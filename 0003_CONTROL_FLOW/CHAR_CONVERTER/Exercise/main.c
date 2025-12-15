4449a1e26789cc5cc0576f5cbc9029d7512e2562
daniellangwost <daniellangwost9@gmail.com>
2025-12-15 16:19:59 +0100

    implement char converter

--- 0003_CONTROL_FLOW/CHAR_CONVERTER/Exercise/main.c
+++ 0003_CONTROL_FLOW/CHAR_CONVERTER/Exercise/main.c
@@ -1,5 +1,5 @@
-#include <stdbool.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int main()
 {
@@ -9,18 +9,29 @@ int main()
     scanf("%c", &character);
 
     // is numeric
+    if (isdigit(character)) 
+        printf("Numeric!\n");
 
     // is upper case
+    if (isupper(character)) 
+        printf("Uppercase!\n");
 
     // is lower case
-
+    if (islower(character)) 
+        printf("Lowercase!\n");
+        
     // is alpha
+    if (isalpha(character)) 
+        printf("Alpha!\n");
 
     // is alpha numeric
+    if (isalnum(character))
+        printf("Alphanum!\n");
 
     // to upper case
-
+    printf("Upper: %c\n", toupper(character));
+    
     // to lower case
-
+    printf("Lower: %c\n", tolower(character));
     return 0;
 }
