e0b0ee9aaa461588d5ee5fb0ae1e88e1da5d9ee4
daniellangwost <daniellangwost9@gmail.com>
2025-12-15 17:03:45 +0100

    implement lanes

--- 0002_VARIABLES/Time_Converter/Exercise/main.c
+++ 0002_VARIABLES/Time_Converter/Exercise/main.c
@@ -2,17 +2,21 @@
 
 int main()
 {
+    // definitions and declarations
     float millisconds = 0.0F;
     float seconds, minutes, hours, days;
 
+    // input
     printf("Please enter a millisecond value: ");
     scanf("%f", &millisconds);
 
+    // computation
     seconds = millisconds / 1000;
     minutes = seconds / 60;
     hours = minutes / 60;
     days = hours / 24;
 
+    // output
     printf("seconds: %f\n", seconds);
     printf("minutes: %f\n", minutes);
     printf("hours: %f\n", hours);
--- 0003_CONTROL_FLOW/LANES_1/Exercise/main.c
+++ 0003_CONTROL_FLOW/LANES_1/Exercise/main.c
@@ -1,9 +1,54 @@
 #include <stdio.h>
 
+void printRoadRow(int choice);
+void printEmptyRoadRow();
+
 int main()
 {
-    printf("Create the properties of a vehicle.\n");
+    // definitions and declarations
+    enum LaneAssociationType
+    {
+        LeftLane = 1,
+        CenterLane = 2,
+        RightLane = 3,
+        UnknownLane = 4
+    };
 
+    float speed;
+    int choice;
+    enum LaneAssociationType currentLane;
+
+    // input
+    printf("Create the properties of a vehicle.\n");
+    printf("Speed in m/s: ");
+    scanf("%f", &speed);
+    printf("Lane (1=Left, 2=Center, 3=Right): ");
+    scanf("%d", &choice);
+    currentLane = choice >= 1 && choice <= 3 ? choice : 4;
+
+    // output
+    printf("                L                 C                 R         \n");
+    printEmptyRoadRow();
+    printRoadRow(choice);
+    printEmptyRoadRow();
 
     return 0;
+}
+
+void printRoadRow(int choice)
+{
+    if (choice == 1)
+        printf("       |        X        |                 |                 |\n");
+    else if (choice == 2)
+        printf("       |                 |        X        |                 |\n");
+    else if (choice == 3)
+        printf("       |                 |                 |        X        |\n");
+    else
+        printf("       |                 |                 |                 |\n");
+
+}
+
+void printEmptyRoadRow()
+{
+    printf("       |                 |                 |                 |\n");
 }
