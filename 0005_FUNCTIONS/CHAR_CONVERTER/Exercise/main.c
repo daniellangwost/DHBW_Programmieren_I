2e741d8c7c6cc72c51a425d5f5e91b252f6100a0
daniellangwost <daniellangwost9@gmail.com>
2026-01-05 15:01:12 +0100

    finish function exercise

--- 0005_FUNCTIONS/CHAR_CONVERTER/Exercise/main.c
+++ 0005_FUNCTIONS/CHAR_CONVERTER/Exercise/main.c
@@ -2,7 +2,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// #include "lib.h"
+#include "lib.h"
+
+void performeTestCases();
 
 int main()
 {
@@ -25,75 +27,6 @@ int main()
     return 0;
 }
 
-bool isNumeric(char character)
-{
-    if ((character >= '0') && (character <= '9'))
-    {
-        return true;
-    };
-
-    return false;
-}
-
-bool isUpperCase(char character)
-{
-    if ((character >= 'A') && (character <= 'Z'))
-    {
-        return true;
-    };
-
-    return false;
-}
-
-bool isLowerCase(char character)
-{
-    if ((character >= 'a') && (character <= 'z'))
-    {
-        return true;
-    };
-
-    return false;
-}
-
-bool isAlpha(char character)
-{
-    if (isUpperCase(character) || isLowerCase(character))
-    {
-        return true;
-    };
-
-    return false;
-}
-
-bool isAlphaNumeric(char character)
-{
-    if (isAlpha(character) || isNumeric(character))
-    {
-        return true;
-    };
-
-    return false;
-}
-
-char toUpperCase(char character)
-{
-    if (isLowerCase(character))
-    {
-        return character - 32;
-    };
-
-    return character;
-}
-
-char toLowerCase(char character)
-{
-    if (isUpperCase(character))
-    {
-        return character + 32;
-    };
-
-    return character;
-}
 
 void performeTestCases()
 {
