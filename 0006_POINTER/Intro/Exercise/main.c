f41b21e9541fb0f632fd5ddc39751a84bdfc73e1
daniellangwost <daniellangwost9@gmail.com>
2026-01-05 15:59:43 +0100

    finish pointer exercise

--- 0006_POINTER/Intro/Exercise/main.c
+++ 0006_POINTER/Intro/Exercise/main.c
@@ -2,7 +2,21 @@
 
 int main()
 {
-    // YOUR CODE HERE //
+    int aValue = 42;
+    int* myPointer = &aValue;
+    printf("Initial Value: \t\t\t\t\t%d\n", *myPointer);
+    printf("Value through pointer: \t\t\t\t%d\n", aValue);
+
+    *myPointer = 20;
+
+    printf("Modified value of aValue through myPointer: \t%d\n", aValue);
+
+    int* yourPointer = myPointer;
+
+    printf("Address of aValue:\t%p\n", &aValue);
+    printf("Address of myPointer:\t%p\n", &myPointer);
+    printf("Address of yourPointer:\t%p\n", &yourPointer);
+
 
     return 0;
 }
