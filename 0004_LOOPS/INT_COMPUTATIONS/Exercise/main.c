3e1dcb14aa6034ecb55c19da17eba2f12c50c2c8
daniellangwost <daniellangwost9@gmail.com>
2026-01-05 14:44:15 +0100

    solve loops exercise

--- 0004_LOOPS/INT_COMPUTATIONS/Exercise/main.c
+++ 0004_LOOPS/INT_COMPUTATIONS/Exercise/main.c
@@ -10,15 +10,26 @@ int main()
 
     // sum of digits
     uint32_t numDigits = 0;
+    uint32_t tempNumber = inputNumber;
 
-    // CODE HERE //
+    while (tempNumber)
+    {
+        numDigits++;
+        tempNumber /= 10;
+    }
+    
 
     printf("sum of digits: %u\n", numDigits);
 
     // cross sum
     uint32_t crossSum = 0;
+    tempNumber = inputNumber;
+    for (uint32_t i = 0; i < numDigits; i++)
+    {
+        crossSum += tempNumber % 10;
+        tempNumber /= 10;
+    }
     
-    // CODE HERE //
 
     printf("crossSum: %u\n", crossSum);
 
