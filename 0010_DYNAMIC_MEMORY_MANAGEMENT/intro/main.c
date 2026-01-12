9edcc106a234dffebfaed927da8dc540c4edeff5
daniellangwost <daniellangwost9@gmail.com>
2026-01-12 15:10:54 +0100

    finish intro

--- 0010_DYNAMIC_MEMORY_MANAGEMENT/intro/main.c
+++ 0010_DYNAMIC_MEMORY_MANAGEMENT/intro/main.c
@@ -3,6 +3,28 @@
 
 int main()
 {
+    size_t length = 3;
+    size_t memoryReq = length * sizeof(int);
 
+    printf("adress of length: %p\n", (void*)&length);
+
+    printf("sizeof(int): %zu\n", sizeof(int));
+    printf("memoryReq: %zu\n", memoryReq);
+
+    int* heapArray = malloc(memoryReq);
+
+    for (size_t i = 0; i < length; i++)
+    {
+        heapArray[i] = (int)i * 100;
+    }
+
+    for (size_t i = 0; i < length; i++)
+    {
+        printf("heapArray[%zu]: %d\n", i, heapArray[i]);
+        printf("address of heapArray[%zu]: %p\n", i, (void*)&heapArray[i]);
+    }
+
+    free(heapArray);
+    heapArray = NULL; // = (void*)0
     return 0;
 }
