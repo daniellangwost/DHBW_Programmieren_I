9a1b477f17018ca5c5115df271429caa72f86a7c
daniellangwost <daniellangwost9@gmail.com>
2026-01-12 15:14:17 +0100

    finish dynamic array exercise

--- 0010_DYNAMIC_MEMORY_MANAGEMENT/Exercise_dynamic_array/main.c
+++ 0010_DYNAMIC_MEMORY_MANAGEMENT/Exercise_dynamic_array/main.c
@@ -3,7 +3,29 @@
 
 int main() 
 {
+    int n = 0;
+    int sum = 0;
+    do
+    {
+        printf("Enter the number of elements: ");
+        scanf("%d", &n);
+        if (n <= 0) printf("Invalid input! The number of elements must be greater than zero.\n");
+    } while (n <= 0);
 
+    int* array = malloc((size_t)n * sizeof(int));
+    if (!array)
+    {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d", &array[i]);
+        sum += array[i];
+    }
+    printf("The sum of the array elements is: %d\n", sum);
+    
 
     return 0;
 }
